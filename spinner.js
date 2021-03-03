6e32866ad30863c0fd7238a29901b4e88ce01618
christopher morris <christopher.pkm@gmail.com>
2021-03-03 06:05:32 +0000

    using request animation frame instead with only drawing after elapsed time in ms > time elapsed in ms since last render, this approch seems smoother visually I believe

--- spinner.js
+++ spinner.js
@@ -1,8 +1,8 @@
 
 
 // api
-function spinner(container, size = 200, radius = 25,
-                 speedFps = 15, dotSize = 4, gapSize = 0.9, nOfDots = 4, dotsColour = "#00ffff"){
+function spinner(container, size = 200, radius = 15,
+                 speedFps = 40, dotSize = 3, gapSize = 2, nOfDots = 3, dotsColour = "#00ffff"){
 
     let c = document.createElement("canvas");
     c.width = size;
@@ -13,9 +13,9 @@ function spinner(container, size = 200, radius = 25,
 
     // initialize state vars
     let i = 0;
-    let gapSizeBetweenDot = 0.9;
+    let gapSizeBetweenDot = gapSize;
     let j = 0;
-    let increment = 0.1;
+    let increment = 0.08;
     let toggle = true;
     const FPS = 60;
     let speedTimerId = null;
@@ -41,32 +41,86 @@ function spinner(container, size = 200, radius = 25,
         });
     }
 
+
     return {
         start: function(){
-            if(renderTimerId == null)
-                renderTimerId = window.setInterval(()=>{
-                    let points = [];
-                    for(let n = 0; n < nOfDots; n++){
-                        points.push(j+(gapSizeBetweenDot*n));
-                    }
+            let fpsInterval, startTime, now, then, elapsed;
 
-                    // let points = [j, j+(gapSizeBetweenDot), j+(gapSizeBetweenDot*2)];
-                    draw(points);
-                }, 1000 / FPS);
+            let step = () => {
+                let points = [];
+                for(let n = 0; n < nOfDots; n++){
+                    points.push(j+(gapSizeBetweenDot*n));
+                }
 
-            if(speedTimerId == null)
-                speedTimerId = window.setInterval(()=>{
-                    j += smoothTanhFunction(i);
-                    if(toggle){
-                        i += increment;
-                        if(i >= +2)
-                            toggle = false;
-                    }else{
-                        i -= increment;
-                        if(i <= -1.4)
-                            toggle = true;
-                    }
-                },1000 / speedFps);
+                // let points = [j, j+(gapSizeBetweenDot), j+(gapSizeBetweenDot*2)];
+                draw(points);
+                j += smoothTanhFunction(i);
+                if(toggle){
+                    i += increment;
+                    if(i >= +5)
+                        toggle = false;
+                }else{
+                    i -= increment;
+                    if(i <= -3)
+                        toggle = true;
+                }
+            }
+
+            function startAnimating(fps) {
+                fpsInterval = 1000 / fps;
+                then = Date.now();
+                startTime = then;
+                animate();
+            }
+
+            // the animation loop calculates time elapsed since the last loop
+// and only draws if your specified fps interval is achieved
+
+            function animate() {
+
+                // request another frame
+
+                requestAnimationFrame(animate);
+
+                // calc elapsed time since last loop
+
+                now = Date.now();
+                elapsed = now - then;
+
+                // if enough time has elapsed, draw the next frame
+
+                if (elapsed > fpsInterval) {
+
+                    // Get ready for next frame by setting then=now, but also adjust for your
+                    // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+                    then = now - (elapsed % fpsInterval);
+
+                    // Put your drawing code here
+                    step();
+
+                }
+            }
+
+            startAnimating(FPS);
+
+            // if(renderTimerId == null)
+            //     renderTimerId = window.requestAnimationFrame();
+
+
+
+            // if(speedTimerId == null)
+            //     speedTimerId = window.setInterval(()=>{
+            //         j += smoothTanhFunction(i);
+            //         if(toggle){
+            //             i += increment;
+            //             if(i >= +2)
+            //                 toggle = false;
+            //         }else{
+            //             i -= increment;
+            //             if(i <= -2)
+            //                 toggle = true;
+            //         }
+            //     },1000 / speedFps);
 
         },
 
@@ -86,7 +140,7 @@ function spinner(container, size = 200, radius = 25,
         }
     }
 }
-
+// \tanh\left(x\cdot0.8\right)\cdot0.5\ +\ 0.6
 function smoothTanhFunction(x){
-    return Math.tanh(x * 2) * 0.4 + 0.6;
+    return Math.tanh(x*0.4) * 0.09 + 0.15;
 }
